cec60dea97f030a47ab0fe983f6da3095a6b0bf1
TretiakovaNatalia <tretyakova0288@gmail.com>
2024-07-12 16:34:45 +0500

    Add simple menu

--- src/SortingResult.java
+++ src/SortingResult.java
@@ -1,11 +1,11 @@
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-@AllArgsConstructor
-public class SortingResult {
-    private Integer[] array;
-    private int count_permutations;
-}
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//
+//@Getter
+//@Builder
+//@AllArgsConstructor
+//public class SortingResult {
+//    private Integer[] array;
+//    private int count_permutations;
+//}
